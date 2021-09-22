c322cf2d139fbfbfc7ca50af542398fa533055bc
joshua <waitingwu.cai09g@nctu.edu.tw>
2021-09-22 22:06:25 +0800

    added os

--- app.py
+++ app.py
@@ -46,6 +46,8 @@ def handle_message(event):
         TextSendMessage(text=event.message.text))
 
 
+import os
+
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
