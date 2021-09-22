b14f9dfab4b7fcbfcee5917d810fdbe2494201b6
joshua <waitingwu.cai09g@nctu.edu.tw>
2021-09-22 22:04:14 +0800

    fixed port

--- app.py
+++ app.py
@@ -47,5 +47,5 @@ def handle_message(event):
 
 
 if __name__ == "__main__":
-    .listen(process.env.PORT || 5000)
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
