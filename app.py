56c5ad33833aee76dbdedb77e335d78b2a708c15
yayafang <76805797+yayafang@users.noreply.github.com>
2021-09-22 12:33:08 +0800

    add line_sdk and write app.py

--- app.py
+++ app.py
@@ -1,44 +1,46 @@
 # -*- coding: utf-8 -*-
 
-from flask import Flask, request, abort
+from flask import Flask, request, jsonify
+
+from line_sdk import Linebot
+
 from IOHbot import runLoki
 
-from linebot import (
-    LineBotApi, WebhookHandler
-)
-from linebot.exceptions import (
-    InvalidSignatureError
-)
-from linebot.models import *
+LINE_ACCESS_TOKEN   = "w477YzEf5ubS1SGrLu88dQvzRdR4HUgbjGvbFPxZ3xVVW0s/1ItqB5z2NaOXk2H8kaQybBx+ICHH8jIkWdCYBTBudh4/+1KzAuQRd6c9DOf/jO0/loUC2rgmAolT5UMx+y07nVc+veZLyWY/aZHpggdB04t89/1O/w1cDnyilFU="
+LINE_CHANNEL_SECRET = "a126d182d8794e0cde2b203d4496a0ef"
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi('w477YzEf5ubS1SGrLu88dQvzRdR4HUgbjGvbFPxZ3xVVW0s/1ItqB5z2NaOXk2H8kaQybBx+ICHH8jIkWdCYBTBudh4/+1KzAuQRd6c9DOf/jO0/loUC2rgmAolT5UMx+y07nVc+veZLyWY/aZHpggdB04t89/1O/w1cDnyilFU=')
-handler = WebhookHandler('a126d182d8794e0cde2b203d4496a0ef')
+@app.route("/callback", methods=["GET", "POST"])
+def webhook():
+    #GET
+    if request.method == "GET":
+        return jsonify({"status": True, "msg": "Line Webhook Success."})
+    
+    #POST
+    elif request.method == "POST":
+        signature = request.headers['X-Line-Signature']
+        body = request.get_data(as_text=True)
+        
+        # Line
+        linebot = Linebot(LINE_ACCESS_TOKEN, LINE_CHANNEL_SECRET)        
+        
+        #dataLIST =[{status, type, message, userID, replyToken, timestamp}]     
+        dataLIST = linebot.parse(body, signature)
+        for dataDICT in dataLIST:
+            if dataDICT["status"]:
+                if dataDICT["type"] == "message":
+                    if dataDICT["message"] == 'Hi':
+                        linebot.respText(dataDICT["replyToken"], "請你告訴我你想找的系")
+                        return
+                    if dataDICT["message"] == '謝謝':
+                        linebot.respText(dataDICT["replyToken"], "期待下次再幫你忙喔！")
+                    else:
+             
 
-line_bot_api.push_message('U27fc746a9ee5264743e7a594c165c2b0', TextSendMessage(text='hello my friend'))
 
-@app.route("/callback", methods=['POST'])
-def callback():
-    # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
+    
 
-    # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-
-    # handle webhook body
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
-
-    return 'OK'
-
-@handler.add(MessageEvent, message=TextMessage)
-#def handle_message(event):
-    #message = TextSendMessage(text=event.message.text)
-    #line_bot_api.reply_message(event.reply_token,message)
 
 
 
